00b861063b80018e88d1d4a493986a5b2023743d
Francesco Torchia <francesco.torchia@suse.com>
2025-12-13 16:42:47 +0100

    Remove copy-pasted cypress files
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- cypress/e2e/po/components/link.po.ts
+++
@@ -1,7 +0,0 @@
-import ComponentPo from '@/cypress/e2e/po/components/component.po';
-
-export default class LinkPo extends ComponentPo {
-  click(force = false): Cypress.Chainable {
-    return this.self().click({ force });
-  }
-}
--- cypress/e2e/po/edit/management.cattle.io.roletemplate.po.ts
+++
@@ -1,7 +0,0 @@
-import RoleEditPo from '@/cypress/e2e/po/edit/role.po';
-
-export default class RoleTemplateEditPo extends RoleEditPo {
-  constructor(clusterId = '_', roleId?: string) {
-    super(clusterId, 'management.cattle.io.roletemplate', roleId);
-  }
-}
