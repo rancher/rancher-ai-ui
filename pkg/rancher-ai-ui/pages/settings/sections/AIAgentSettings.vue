0702896c81197346e337518189dce27ed982fa5f
Francesco Torchia <francesco.torchia@suse.com>
2026-03-09 18:26:52 +0100

    Add missing Bedrock regions
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/rancher-ai-ui/pages/settings/sections/AIAgentSettings.vue
+++ pkg/rancher-ai-ui/pages/settings/sections/AIAgentSettings.vue
@@ -45,15 +45,38 @@ const emit = defineEmits(['update:value', 'update:validation-error']);
 const { fetchLLMModels } = useChatApiComposable();
 
 const BEDROCK_REGION_OPTIONS = [
-  'us-east-1',
-  'us-west-2',
-  'eu-west-1',
-  'eu-central-1',
-  'eu-north-1',
-  'ap-southeast-1',
-  'ap-northeast-1',
-  'ap-south-1',
-  'ca-central-1'
+  // North America
+  'us-east-1',      // US East (N. Virginia)
+  'us-east-2',      // US East (Ohio)
+  'us-west-2',      // US West (Oregon)
+  'ca-central-1',   // Canada (Central)
+  'us-gov-west-1',  // AWS GovCloud (US-West)
+
+  // Europe
+  'eu-central-1',   // Europe (Frankfurt)
+  'eu-west-1',      // Europe (Ireland)
+  'eu-west-2',      // Europe (London)
+  'eu-west-3',      // Europe (Paris)
+  'eu-north-1',     // Europe (Stockholm)
+  'eu-south-1',     // Europe (Milan)
+  'eu-central-2',   // Europe (Zurich)
+
+  // Asia Pacific
+  'ap-southeast-1', // Asia Pacific (Singapore)
+  'ap-southeast-2', // Asia Pacific (Sydney)
+  'ap-southeast-3', // Asia Pacific (Jakarta)
+  'ap-northeast-1', // Asia Pacific (Tokyo)
+  'ap-northeast-2', // Asia Pacific (Seoul)
+  'ap-northeast-3', // Asia Pacific (Osaka)
+  'ap-south-1',     // Asia Pacific (Mumbai)
+  'ap-east-1',      // Asia Pacific (Hong Kong)
+
+  // South America
+  'sa-east-1',      // South America (São Paulo)
+
+  // Middle East & Africa
+  'me-central-1',   // Middle East (UAE)
+  'af-south-1'      // Africa (Cape Town)
 ];
 
 const activeChatbotOptions = [
